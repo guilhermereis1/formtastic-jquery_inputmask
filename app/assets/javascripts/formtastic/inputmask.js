f8f416924076082b2c7a2c9db69e9f5f44ea868a
Guilherme Reis <guilhermejosereis27@gmail.com>
2019-02-04 14:10:13 -0300

    Initial Commit

---
+++ app/assets/javascripts/formtastic/inputmask.js
@@ -0,0 +1,7 @@
+//= require inputmask/inputmask
+//= require inputmask/jquery.inputmask
+//= require inputmask/inputmask.extensions
+//= require inputmask/inputmask.date.extensions
+//= require inputmask/inputmask.numeric.extensions
+//= require inputmask/inputmask.phone.extensions
+//= require inputmask/bindings/inputmask.binding
---
+++ lib/formtastic/jquery_inputmask/version.rb
@@ -0,0 +1,10 @@
+module Formtastic
+  module JqueryInputmask
+    module Version
+      MAJOR = 0
+      MINOR = 2
+      PATCH = 2
+      STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
+    end
+  end
+end
